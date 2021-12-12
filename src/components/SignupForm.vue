9a4e3c9074f19209979c43ea628dd1b176b723ac
ryuya416 <67462677+ryuya416@users.noreply.github.com>
2021-12-12 13:41:57 +0900

    outputting errors

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -8,6 +8,7 @@
     />
     <input type="email" required placeholder="email" v-model="email" />
     <input type="password" required placeholder="password" v-model="password" />
+    <div class="error">{{ error }}</div>
     <button>Sign up</button>
   </form>
 </template>
@@ -25,9 +26,11 @@ export default {
     const { error, signup } = useSignup();
     const handleSubmit = async () => {
       await signup(email.value, password.value, displayName.value);
-      console.log("user signed up");
+      if (!error.value) {
+        console.log("user signed up");
+      }
     };
-    return { displayName, email, password, handleSubmit };
+    return { displayName, email, password, handleSubmit, error };
   },
 };
 </script>
